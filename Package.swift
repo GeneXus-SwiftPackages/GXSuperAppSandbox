09c21fe00c744859cdef4baeefc4b55a080c40f3
Build Er <builder@macmini6.local>
2023-05-04 15:12:39 -0300

    Package version: 1.0.0-beta+20230504180356

--- Package.swift
+++ Package.swift
@@ -17,14 +17,14 @@ let package = Package(
 		.target(name: "GXSuperAppSandboxWrapper",
 				dependencies: [
 					"GXSuperAppSandbox",
-					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS, .tvOS])),
-					.product(name: "GXSuperApp", package: "GXSuperApp", condition: .when(platforms: [.iOS, .tvOS]))
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.tvOS, .iOS])),
+					.product(name: "GXSuperApp", package: "GXSuperApp", condition: .when(platforms: [.tvOS, .iOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXSuperAppSandbox",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXSuperAppSandbox-1.0.0-beta+20230503140111.xcframework.zip",
-			checksum: "9e20680873bd9a09fc7d0fed649ba9bdf38bd00de2b0dd172924e41ed2ca6bfa"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXSuperAppSandbox-1.0.0-beta+20230504180356.xcframework.zip",
+			checksum: "48c748323a1d577c0fc51a9f5e7810fcf488785d0b6d1acd86bf2b0d2ea4f5b5"
 		)
 	]
 )
