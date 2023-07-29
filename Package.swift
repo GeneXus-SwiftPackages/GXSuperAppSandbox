5b95493ed5757772cd13a1744c790e23c6335abd
genexusbot <builder@genexus.com>
2023-07-29 18:45:54 -0300

    Package version: 1.1.0-rc.1

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+	name: "GXSuperAppSandbox",
+	platforms: [.iOS("12.0"), .tvOS("12.0")],
+	products: [
+		.library(
+			name: "GXSuperAppSandbox",
+			targets: ["GXSuperAppSandboxWrapper"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.1.0-rc.1"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXSuperApp.git", exact: "1.1.0-rc.1")
+	],
+	targets: [
+		.target(name: "GXSuperAppSandboxWrapper",
+				dependencies: [
+					"GXSuperAppSandbox",
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.tvOS, .iOS])),
+					.product(name: "GXSuperApp", package: "GXSuperApp", condition: .when(platforms: [.tvOS, .iOS]))
+				],
+				path: "Sources"),
+		.binaryTarget(
+			name: "GXSuperAppSandbox",
+			url: "https://pkgs.genexus.dev/iOS/releases/GXSuperAppSandbox-1.1.0-rc.1.xcframework.zip",
+			checksum: "7e82a6e444a8d39b8794fa4d0dfb9f716d71df94283b9a5bb981cc24fd1017f0"
+		)
+	]
+)
