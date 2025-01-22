3e4b9a254e594d201b540ec8eda0a9daf3c77031
The iOS Team @ GeneXus <builder@genexus.com>
2025-01-21 21:15:17 -0300

    Package version: 3.0.0-beta.9

--- Package.swift
+++ Package.swift
@@ -3,15 +3,15 @@ import PackageDescription
 
 let package = Package(
 	name: "GXSuperAppSandbox",
-	platforms: [.iOS("13.0"), .tvOS("13.0"), .visionOS("1.0")],
+	platforms: [.iOS("15.0"), .tvOS("18.0"), .visionOS("2.0")],
 	products: [
 		.library(
 			name: "GXSuperAppSandbox",
 			targets: ["GXSuperAppSandboxWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "3.0.0-beta.8"),
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXSuperApp.git", exact: "3.0.0-beta.8")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "3.0.0-beta.9"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXSuperApp.git", exact: "3.0.0-beta.9")
 	],
 	targets: [
 		.target(name: "GXSuperAppSandboxWrapper",
@@ -23,8 +23,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXSuperAppSandbox",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXSuperAppSandbox-3.0.0-beta.8.xcframework.zip",
-			checksum: "c7d4219d4168f18cae6777fd6d16caaa5e470b1039a9863e35cb8499581c03a1"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXSuperAppSandbox-3.0.0-beta.9.xcframework.zip",
+			checksum: "f5c8be5c0969ec5f6fa195c6673610e4a07ce26fb1d10cc49db25f3fb917d1cc"
 		)
 	]
 )
