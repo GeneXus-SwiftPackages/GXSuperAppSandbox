e72b2b05f7286d9e682d708860a76a6c0137bfe9
The iOS Team @ GeneXus <builder@genexus.com>
2025-12-12 11:17:55 -0500

    Package version: 4.0.5

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
 			targets: ["GXSuperAppSandboxWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "4.0.4"),
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXSuperApp.git", exact: "4.0.4")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "4.0.5"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXSuperApp.git", exact: "4.0.5")
 	],
 	targets: [
 		.target(name: "GXSuperAppSandboxWrapper",
@@ -23,8 +23,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXSuperAppSandbox",
-			url: "https://pkgs.genexus.dev/iOS/releases/GXSuperAppSandbox-4.0.4.xcframework.zip",
-			checksum: "a4dfc1ee46ea371141c22c271017954ed23ee20d8ae71be667f400db6980dd55"
+			url: "https://pkgs.genexus.dev/iOS/releases/GXSuperAppSandbox-4.0.5.xcframework.zip",
+			checksum: "9c6cf9f7bcb16f47f0e57d4786965ee02c336e727f9d3be2d41872b65e877533"
 		)
 	]
 )
